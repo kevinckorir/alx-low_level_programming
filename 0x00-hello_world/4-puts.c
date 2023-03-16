6c2c8768c1d8176949964e99aa26365a5b5587dd
Kevin Korir <korirckevin@gmail.com>
2023-03-16 15:22:30 +0300

    Modified 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*print programming is like building...using puts*/
+int main(void)
+{
+	char str[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(str);
+	return (0);
+}
+
