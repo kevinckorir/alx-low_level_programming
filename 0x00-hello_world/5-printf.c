6effdb6a8c2ea1d00414e4afb4b7bffc2c16fa82
Kevin Korir <korirckevin@gmail.com>
2023-03-16 15:38:41 +0300

    modified 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+*main -Print some line to the standard output
+*
+*we use a printf
+*Return: 0
+*/
+int main(void)
+{
+	/*Display he string inside quotations*/
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
