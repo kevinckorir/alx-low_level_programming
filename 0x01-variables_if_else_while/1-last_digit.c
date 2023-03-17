a8622b0e55ca2c365812149043649ea744b51a23
Kevin Korir <korirckevin@gmail.com>
2023-03-17 09:29:52 +0300

    modified

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * no parameters
+ *
+ * this function checks for some strings
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() -RAND_MAX / 2;
+	
+	return (0);
