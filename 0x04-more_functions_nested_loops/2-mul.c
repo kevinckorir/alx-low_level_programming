3f9801f5b33642702f1d36bdf7e6cf7798330d11
Kevin Korir <korirckevin@gmail.com>
2023-03-23 09:43:08 +0300

    Add source code file 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: takes one integer as parameter
+ * @b: second integer param
+ * Return: multiple of the two parameters.
+ */
+int mul(int a, int b)
+{
+	int mul = a * b;
+
+	return (mul);
+}
