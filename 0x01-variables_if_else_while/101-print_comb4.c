93276bcce14d4f7d8e59b7f649dfcecd917197df
Kevin Korir <korirckevin@gmail.com>
2023-03-26 17:34:33 +0300

    Added 101-print_comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of three digit numbers
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			for (k = j + 1; k < 10; k++)
+			{
+			putchar(i + '0');
+			putchar(j + '0');
+			putchar(k + '0');
+			
+			if (i != 7 || j != 8 || k != 9)
+			{
+			putchar(',');
+			putchar(' ');
+			}
+			}
+			}
+	}
+	
+	putchar('\n');
+	return (0);
+}
