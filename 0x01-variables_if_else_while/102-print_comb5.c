2328db215e5fb819e37b129b6ad94cb34c02c489
Kevin Korir <korirckevin@gmail.com>
2023-03-26 18:34:55 +0300

    Made changes to 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,29 +11,19 @@ int main(void)
 
 	for (i = 0; i < 100; i++)
 	{
-		for (j = 0; j < 100; j++)
+		for (j = i + 1; j < 100; j++)
 		{
-		if (i != j)
-		{
-		/*if (i > j)
-		{
-		int temp = i;
-
-		i = j;
-		j = temp;
-		}*/
-		putchar(i / 10 + '0');
-		putchar(i % 10 + '0');
-		putchar(' ');
-		putchar(j / 10 + '0');
-		putchar(j % 10 + '0');
-
-		if (i != 98 || j != 99)
-		{
-			putchar(',');
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
 			putchar(' ');
-		}
-		}
+			putchar(j / 10 + '0');
+			putchar(j % 10 + '0');
+
+			if (i != 98 || j != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 	putchar('\n');
