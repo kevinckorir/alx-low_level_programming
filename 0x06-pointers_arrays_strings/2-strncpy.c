7a6b681312a7b76804af20c9c4d5b54dbfdd2319
Kevin Korir <korirckevin@gmail.com>
2023-03-30 18:16:18 +0300

    Made corrections to 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,14 +4,14 @@
  * _strncpy - copy string
  * @dest: destination where the string is copied to
  * @src: source of the string being copied
- * @n: size in bytes of source
+ * @n: max number of characters to copy
  * Return: pointer to the dest
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	char *dest_origin = dest;
 
-	while (*src != '\0' && *src < n)
+	while (*src != '\0' && n--)
 	{
 		*dest = *src;
 		dest++;
