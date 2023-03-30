8c027f5b71544231176e3ef3b655b7e6a3dd957c
Kevin Korir <korirckevin@gmail.com>
2023-03-30 19:09:01 +0300

    made corrections to 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -15,6 +15,6 @@ int _strcmp(char *s1, char *s2)
 		s1++;
 		s2++;
 	}
-	compare = (*s1 == *s2) ? 0 : (*s1 > *s2) ? 1 : -1;
+	compare = (*s1 == *s2) ? 0 : (*s1 > *s2) ? 15 : -15;
 	return (compare);
 }
