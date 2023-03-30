f8e1475a0e5d3046e4a52c1f3f50bdcecc3e4222
Kevin Korir <korirckevin@gmail.com>
2023-03-30 18:35:28 +0300

    Made changes to on formatting

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,12 +11,15 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	char *start = dest;
 
-	
-	while (*src && n--)
+	while (*src != '\0' && n-- > 0)
 	{
 		*dest = *src;
 		dest++;
 		src++;
+	}
+	while (n-- > 0)
+	{
+		*dest++ = '\0';
 	}
 	*dest = '\0';
 	return (start);
