daea42e2a8d82312afaaee1be1e08ff98ae6edaa
Kevin Korir <korirckevin@gmail.com>
2023-03-31 23:14:27 +0300

    Added 6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * cap_string - capitalize all words of a string
+ * @str: str to be capitalized
+ * Return: capitalized str
+ */
+char *cap_string(char *str)
+{
+	int cap_following_char = 1;
+	int k;
+
+	for (k = 0; str[k] != '\0'; k++)
+	{
+		if (isspace(str[k]) || ispunct(str[k]))
+		{
+			cap_following_char = 1;
+		}
+		else if (cap_following_char)
+		{
+			str[k] = toupper(str[k]);
+			cap_following_char = 0;
+		}
+	}
+	return (str);
+}
