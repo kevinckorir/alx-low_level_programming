19b4c204222ed1fc9c5418a2a1f52867b07018d5
Kevin Korir <korirckevin@gmail.com>
2023-04-05 21:53:49 +0300

    Added READMe to root directory

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _puts_recursion - prints a string 
+ * @s: string parameter
+ * Return: Nothing
+ */
+void _puts_recursion(char *s)
+{
+	if (strlen(s) == 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(s[0]);
+	_puts_recursion(s + 1);
+}
