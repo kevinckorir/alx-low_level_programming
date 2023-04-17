7bec450ac565278ed69b27ab37ff308d88770dcd
Kevin Korir <korirckevin@gmail.com>
2023-04-17 20:35:51 +0300

    added 1-init.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,14 +11,9 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d != NULL)
-	{
-		d->name = malloc(strlen(name) + 1);
-		d->owner = malloc(strlen(owner) + 1);
-		strcpy(d->name, name);
-		d->age = age;
-		strcpy(d->owner, owner);
-	}
-	free(d->name);
-	free(d->owner);
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
