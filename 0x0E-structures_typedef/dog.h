a5c7bcf1989f89beb380a34133e814fd353f6e30
Kevin Korir <korirckevin@gmail.com>
2023-04-17 18:39:51 +0300

    made corrections to dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
 
+/**
+ * struct dog - define description for dog
+ * @name: pointer to string containing name
+ * @age: age of dog
+ * @owner: pointer to string containing owner
+ */
 struct dog
 {
 char *name;
 float age;
 char *owner;
 };
+
+#endif
