c0c3e3eb83a133e9d9c524abfc5024cf2a58886d
Kevin Korir <korirckevin@gmail.com>
2023-04-25 17:24:34 +0300

    Made corrections to 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,14 @@
-#include "main.h"
+/*#include "main.h"*/
 #include <stdio.h>
 
-/** main -prints its name
+/**
+ * main - prints its name
+ * @argc: first param
+ * @argv: second param
  * Return: 0 success
  */
-int main()
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s\n", __FILE__);
+	printf("%s\n", argv[0]);
 	return (0);
 }
